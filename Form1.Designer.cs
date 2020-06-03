3f7c82ef8c710c79ec8a9bf7edbf5f789dafeb45
niks.janeks@gmail.com <niks.janeks@gmail.com>
2020-06-03 17:11:56 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace TrafficLights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RedLight = new System.Windows.Forms.PictureBox();
+            this.YellowLight = new System.Windows.Forms.PictureBox();
+            this.GreenLight = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.RedLight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellowLight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenLight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RedLight
+            // 
+            this.RedLight.Location = new System.Drawing.Point(40, 12);
+            this.RedLight.Name = "RedLight";
+            this.RedLight.Size = new System.Drawing.Size(150, 150);
+            this.RedLight.TabIndex = 0;
+            this.RedLight.TabStop = false;
+            this.RedLight.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // YellowLight
+            // 
+            this.YellowLight.Location = new System.Drawing.Point(40, 168);
+            this.YellowLight.Name = "YellowLight";
+            this.YellowLight.Size = new System.Drawing.Size(150, 150);
+            this.YellowLight.TabIndex = 0;
+            this.YellowLight.TabStop = false;
+            this.YellowLight.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // GreenLight
+            // 
+            this.GreenLight.Location = new System.Drawing.Point(40, 324);
+            this.GreenLight.Name = "GreenLight";
+            this.GreenLight.Size = new System.Drawing.Size(150, 150);
+            this.GreenLight.TabIndex = 0;
+            this.GreenLight.TabStop = false;
+            this.GreenLight.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 625);
+            this.Controls.Add(this.GreenLight);
+            this.Controls.Add(this.YellowLight);
+            this.Controls.Add(this.RedLight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.RedLight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellowLight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenLight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox RedLight;
+        private System.Windows.Forms.PictureBox YellowLight;
+        private System.Windows.Forms.PictureBox GreenLight;
+    }
+}
+
