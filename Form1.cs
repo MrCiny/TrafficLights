1eb996595ec5c2e22614ae121a0923cf22ab6d44
niks.janeks@gmail.com <niks.janeks@gmail.com>
2020-06-10 15:55:04 +0300

    yes

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,90 @@ using System.Windows.Forms;
 
 namespace TrafficLights
 {
-    public partial class Form1 : Form
+    public partial class TrafficLights : Form
     {
-        public Form1()
+        private Timer timerSwitch = null;
+        private Timer timerBlink= null;
+        private int timeCounter = 0;
+        private PictureBox lightToBlink = null;
+        public TrafficLights()
         {
             InitializeComponent();
+            InitializeTrafficLights();
+            InitializeTimerSwitch();
+            InitializeTimerBlink();
+        }
+
+        private void InitializeTimerSwitch()
+        {
+            timerSwitch = new Timer();
+            timerSwitch.Interval = 1000;
+            timerSwitch.Tick += new EventHandler(TimerSwitch_Tick);
+            timerSwitch.Start();
+        }
+
+        private void InitializeTimerBlink()
+        {
+            timerBlink = new Timer();
+            timerBlink.Interval = 200;
+            timerBlink.Tick += new EventHandler(TimerBlink_Tick);
+        }
+
+        private void InitializeTrafficLights()
+        {
+            RedLight.BackColor = Color.Gray;
+            YellowLight.BackColor = Color.Gray;
+            GreenLight.BackColor = Color.Gray;
+        }
+
+        private void TimerSwitch_Tick(object sender, EventArgs e)
+        {
+            SwitchLights();
+        } 
+
+        private void TimerBlink_Tick(object sender, EventArgs e)
+        {
+            if(GreenLight.BackColor == Color.Gray)
+            {
+                GreenLight.BackColor = Color.Green;
+            }
+            else
+            {
+                GreenLight.BackColor = Color.Gray;
+            }
+        }
+
+        private void SwitchLights()
+        {
+            switch (timeCounter)
+            {
+                case 0:
+                    RedLight.BackColor = Color.Red;
+                    break;
+                case 3:
+                    YellowLight.BackColor = Color.Yellow;
+                    //RedLight.BackColor = Color.Gray;
+                    break;
+                case 5:
+                    YellowLight.BackColor = Color.Gray;
+                    GreenLight.BackColor = Color.Green;
+                    RedLight.BackColor = Color.Gray;
+                    break;
+                case 6:
+                    timerBlink.Start();
+                    break;
+                case 8:
+                    timerBlink.Stop();
+                    YellowLight.BackColor = Color.Yellow;
+                    GreenLight.BackColor = Color.Gray;
+                    break;
+                case 10:
+                    YellowLight.BackColor = Color.Gray;
+                    RedLight.BackColor = Color.Red;
+                    timeCounter = -1;
+                    break;
+            }
+            timeCounter++;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
